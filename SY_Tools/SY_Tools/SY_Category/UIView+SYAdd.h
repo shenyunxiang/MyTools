c6a17c743eac2c34fa3a39ffdeec41caed2c02e4
shenxunxiang <shenyunxiang@shenxunxiangdeMacBook-Pro.local>
2016-04-22 20:12:26 +0800

    添加UIView的分类

---
+++ SY_Tools/SY_Tools/SY_Category/UIView+SYAdd.h
@@ -0,0 +1,16 @@
+//
+//  UIView+SYAdd.h
+//  SY_Tools
+//
+//  Created by shenxunxiang on 16/4/22.
+//  Copyright © 2016年 shenyunxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+
+@interface UIView (SYAdd)
+
+@end
---
+++ SY_Tools/SY_Tools/SY_Category/UIView+SYAdd.m
@@ -0,0 +1,13 @@
+//
+//  UIView+SYAdd.m
+//  SY_Tools
+//
+//  Created by shenxunxiang on 16/4/22.
+//  Copyright © 2016年 shenyunxiang. All rights reserved.
+//
+
+#import "UIView+SYAdd.h"
+
+@implementation UIView (SYAdd)
+
+@end
