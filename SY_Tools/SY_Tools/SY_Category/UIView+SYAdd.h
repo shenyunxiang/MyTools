f7a23c1c7ef86d22f659b6071835d39d927e07da
shenxunxiang <shenyunxiang@shenxunxiangdeMacBook-Pro.local>
2016-04-23 00:25:59 +0800

    frame  +  capture image

--- SY_Tools/SY_Tools/SY_Category/UIView+SYAdd.h
+++ SY_Tools/SY_Tools/SY_Category/UIView+SYAdd.h
@@ -8,9 +8,35 @@
 
 #import <UIKit/UIKit.h>
 
-
-
+#define kscreenSize     [UIScreen mainScreen].bounds.size
+#define kfitWidth(w)    w * (kscreenSize.width / 320.0f)
+#define kfitHeight(h)   h * (kscreenSize.height / 568.0f)
 
 @interface UIView (SYAdd)
+
+// frame
+@property(nonatomic, assign) CGFloat    top;
+
+@property(nonatomic, assign) CGFloat    left;
+
+@property(nonatomic, assign) CGFloat    bottom;
+
+@property(nonatomic, assign) CGFloat    right;
+
+@property(nonatomic, assign) CGFloat    width;
+
+@property(nonatomic, assign) CGFloat    height;
+
+@property(nonatomic, assign) CGFloat    centerX;
+
+@property(nonatomic, assign) CGFloat    centerY;
+
+//UIImage
+- (nullable UIImage *)snapshotImage;
+
+- (void)removeAllSubviews;
+
+@property (nullable, nonatomic, readonly) UIViewController *viewController;
+
 
 @end
--- SY_Tools/SY_Tools/SY_Category/UIView+SYAdd.m
+++ SY_Tools/SY_Tools/SY_Category/UIView+SYAdd.m
@@ -10,4 +10,111 @@
 
 @implementation UIView (SYAdd)
 
+#pragma mark ---- frame ----
+- (CGFloat)top{
+    return self.frame.origin.x;
+}
+
+- (void)setTop:(CGFloat)top{
+    CGRect frame = self.frame;
+    frame.origin.y = top;
+    self.frame = frame;
+}
+
+- (CGFloat)left{
+    return self.frame.origin.y;
+}
+
+- (void)setLeft:(CGFloat)left{
+    CGRect frame = self.frame;
+    frame.origin.x = left;
+    self.frame = frame;
+}
+
+- (CGFloat)bottom{
+    return self.frame.origin.y + self.frame.size.height;
+}
+
+- (void)setBottom:(CGFloat)bottom{
+    CGRect frame = self.frame;
+    frame.origin.y = bottom - self.frame.size.height;
+    self.frame = frame;
+}
+
+- (CGFloat)right{
+    return self.frame.origin.y + self.frame.size.width;
+}
+
+-(void)setRight:(CGFloat)right{
+    CGRect frame = self.frame;
+    frame.origin.x = right - self.frame.size.width;
+    self.frame = frame;
+}
+
+- (CGFloat)width{
+    return self.frame.size.width;
+}
+
+-(void)setWidth:(CGFloat)width{
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+
+- (CGFloat)height{
+    return self.frame.size.height;
+}
+
+-(void)setHeight:(CGFloat)height{
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
+}
+
+- (CGFloat)centerX{
+    return self.center.x;
+}
+
+- (void)setCenterX:(CGFloat)centerX{
+    CGPoint center = self.center;
+    center.x = centerX;
+    self.center = center;
+}
+
+- (CGFloat)centerY{
+    return  self.center.y;
+}
+
+
+- (void)setCenterY:(CGFloat)centerY{
+    CGPoint center = self.center;
+    center.y = centerY;
+    self.center = center;
+}
+
+#pragma mark ----- UIImage -----
+- (UIImage *)snapshotImage{
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, 0);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *snap = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return snap;
+}
+
+- (void)removeAllSubviews{
+    while (self.subviews.count) {
+        [self.subviews.lastObject removeFromSuperview];
+    }
+}
+
+- (UIViewController *)viewController {
+    for (UIView *view = self; view; view = view.superview) {
+        UIResponder *nextResponder = [view nextResponder];
+        if ([nextResponder isKindOfClass:[UIViewController class]]) {
+            return (UIViewController *)nextResponder;
+        }
+    }
+    return nil;
+}
+
 @end
